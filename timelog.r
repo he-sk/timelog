c9d2f184196a5a78d1fbed5ed428e4c16e6ce082
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2018-01-04 04:34:57 +0100

    Fix wrapping at end of year

--- timelog.r
+++ timelog.r
@@ -57,7 +57,7 @@ ds <- within(ds, ActivityType <- factor(ActivityType, levels=order))
 ds$Date <- sprintf("%4d-%02d-%02d", year(ds$Start), month(ds$Start), day(ds$Start))
 ds$Weekday <- wday(ds$Start)
 ds$Week <- isoweek(ds$Start)
-ds$WeekLabel <- sprintf("%d: %s", isoweek(ds$Date), ymd(ds$Date) - wday(ds$Date) + 1)
+ds$WeekLabel <- sprintf("%s (%d)", ymd(ds$Date) - wday(ds$Date) + 1, isoweek(ds$Date))
 ds$Duration <- interval(ds$Start, ds$End) %/% minutes(1)
 
 # compute range
@@ -67,21 +67,31 @@ start <- ymd(end) - weeks * 7 + 1
 
 plot_daily_detail <- function(ds) {
 
+  # restrict
+  ds <- subset(ds, Date >= start)
+
   # compute labels
-  breaks <- seq(yday(start), yday(end), 7)
   labels <- seq(ymd(start), ymd(end), 7)
   labels <- sprintf("%s.%s.", day(labels), month(labels))
   
+  ds$Index <- as.numeric(ymd(ds$Date) - start)
+  breaks <- seq(0, (weeks - 1) * 7, 7)
+
+  # compute colors
+  used_colors <- data.frame(ActivityType=order, Color=colors)
+  used_colors <- subset(used_colors, ActivityType %in% ds$ActivityType, select=c("Color"))
+  used_colors <- as.character(used_colors$Color)
+
   # plot timelog
   p <- ggplot(ds, aes(xmin=60*hour(Start)+minute(Start), 
                       xmax=60*hour(End)+minute(End), 
-                      ymin=yday(Start)-0.5,
-                      ymax=yday(Start)+0.5,
+                      ymin=Index-0.5,
+                      ymax=Index+0.5,
                       fill=ActivityType)) 
   p <- p + geom_rect(color="#808080", size=0.1)
   p <- p + scale_x_continuous(expand=c(0, 0), limit=c(0, 1440), breaks=seq(0, 1440, 60), labels=seq(0, 24, 1))
-  p <- p + scale_y_reverse(expand=c(0, 0), limit=c(yday(end) + 0.5, yday(start) - 0.5), breaks=breaks, labels=labels)
-  p <- p + scale_fill_manual("", values=colors)
+  p <- p + scale_y_reverse(expand=c(0, 0), limit=c(27+0.5, 0-0.5), breaks=breaks, labels=labels)
+  p <- p + scale_fill_manual("", values=used_colors)
   p <- p + theme_bw()
   p <- p + theme(panel.grid.major.y = element_blank(),
                  panel.grid.minor.x = element_blank(), 
@@ -91,11 +101,11 @@ plot_daily_detail <- function(ds) {
 
 plot_daily_summary <- function(ds) {
   # group by day
-  ds <- ddply(ds, .(ActivityType, WeekLabel, Week, Weekday), summarize, Duration=sum(Duration)/60)
+  ds <- ddply(ds, .(ActivityType, WeekLabel, Week, Weekday, Date), summarize, Duration=sum(Duration)/60)
   ds$Label <- ifelse(ds$Duration < 0.5, "", sprintf("%.0f:%02.0f", floor(ds$Duration), (ds$Duration - floor(ds$Duration)) * 60))
   
   # restrict to previous weeks
-  ds <- subset(ds, Week >= isoweek(start))
+  ds <- subset(ds, Date >= start)
   
   # compute colors
   used_colors <- data.frame(ActivityType=order, Color=colors)
@@ -120,12 +130,13 @@ plot_daily_summary <- function(ds) {
 }
 
 plot_weekly_summary <- function(ds) {
-  # group by day
-  ds <- ddply(ds, .(ActivityType, Week), summarize, Duration=sum(Duration)/(60*7))
-  ds$Label <- ifelse(ds$Duration < 0.5, "", sprintf("%.0f:%02.0f", floor(ds$Duration), (ds$Duration - floor(ds$Duration)) * 60))
-  
+
   # restrict to previous weeks
-  ds <- subset(ds, Week >= isoweek(end - weeks(26)))
+  ds <- subset(ds, Date >= end - weeks(26))
+
+  # group by day
+  ds <- ddply(ds, .(ActivityType, WeekLabel, Week), summarize, Duration=sum(Duration)/(60*7))
+  ds$Label <- ifelse(ds$Duration < 0.5, "", sprintf("%.0f:%02.0f", floor(ds$Duration), (ds$Duration - floor(ds$Duration)) * 60))
   
   # compute colors
   used_colors <- data.frame(ActivityType=order, Color=colors)
@@ -133,11 +144,11 @@ plot_weekly_summary <- function(ds) {
   used_colors <- as.character(used_colors$Color)
   
   # plot timelog
-  p <- ggplot(ds, aes(x=Week, y=Duration, fill=ActivityType)) 
+  p <- ggplot(ds, aes(x=WeekLabel, y=Duration, fill=ActivityType)) 
   p <- p + geom_bar(stat="identity", color="black", size=0.1, width=0.9)
   p <- p + geom_text(aes(label=Label), size = 3, position = position_stack(vjust = 0.5))
   p <- p + geom_text(data=subset(ds, ActivityType=="Prokrastination"), aes(label=Label), size = 3, position = position_stack(vjust = 0.5), color="white")
-  p <- p + scale_x_continuous("Week", expand=c(0, 0))
+  p <- p + scale_x_discrete("Week", expand=c(0, 0), labels=unique(ds$Week))
   p <- p + scale_y_continuous("Hours", expand=c(0, 0), breaks=seq(0, 24, 6), limit=c(0, 24))
   p <- p + scale_fill_manual("", values=used_colors)
   p <- p + theme_bw()
